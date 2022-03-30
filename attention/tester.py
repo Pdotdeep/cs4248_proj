a001527c3de24cf66a2534cf152352e06200ec7a
Pdotdeep <paulroopson.pradeep@gmail.com>
2022-03-31 02:20:28 +0800

    changed tester code

--- attention/tester.py
+++ attention/tester.py
@@ -4,11 +4,25 @@ import unicodedata
 import string
 import re
 import random
-
+import contractions
+import nltk
 import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
+import pandas as pd
+import json
+import math
+
+
+import time
+import math
+
+
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.ticker as ticker
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -17,137 +31,69 @@ SOS_token = 0
 EOS_token = 1
 
 
-class Lang:
-    def __init__(self, name):
-        self.name = name
+MAX_LENGTH = 600
+
+
+class Vocabulary:
+    def __init__(self):
         self.word2index = {}
         self.word2count = {}
         self.index2word = {0: "SOS", 1: "EOS"}
         self.n_words = 2  # Count SOS and EOS
 
-    def addSentence(self, sentence):
+    def add_to_vocab(self, sentence):
         for word in sentence.split(' '):
-            self.addWord(word)
+            if word not in self.word2index:
+                self.word2index[word] = self.n_words
+                self.word2count[word] = 1
+                self.index2word[self.n_words] = word
+                self.n_words += 1
+            else:
+                self.word2count[word] += 1
 
-    def addWord(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.word2count[word] = 1
-            self.index2word[self.n_words] = word
-            self.n_words += 1
-        else:
-            self.word2count[word] += 1
+    def sentence_to_indexes(self, sentence):
+        indexes = [self.word2index[word] for word in sentence.split(' ')]
+        indexes.append(EOS_token)
+        return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+    def get_tensor_for_data(self, pair):
+        input_tensor = self.sentence_to_indexes(pair[0])
+        target_tensor = self.sentence_to_indexes(pair[1])
+        return (input_tensor, target_tensor)
 
 
-# Turn a Unicode string to plain ASCII, thanks to
-# https://stackoverflow.com/a/518232/2809427
-def unicodeToAscii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-    )
+def preprocessString(text):
+    text = text.lower().strip()
+    text = contractions.fix(text)
+    text = re.sub(r"([.!?])", r" \1", text)
+    text = re.sub(r"[^a-zA-Z.!?]+", r" ", text)
+    return text
 
-# Lowercase, trim, and remove non-letter characters
-
-import math
-def normalizeString(s):
+def read_data_and_vocab(data_path):
+    print("Getting Data...")
+    vocab = Vocabulary()
     
-    s = unicodeToAscii(s.lower().strip())
-    s = re.sub(r"([.!?])", r" \1", s)
-    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
-    return s
+    data = []
+    collated_df = pd.read_json(data_path)
 
-
-import pandas as pd
-
-import json
-
-
-def read_news(src):
-    print("Reading lines...")
-    
-    pairs = []
-    
-    collated_df = pd.read_json(src)
-
-    print(collated_df)
-    
-    
     for index, row in collated_df.iterrows():
 
-        text = normalizeString(row["text"])
-        title = normalizeString(row["title"])
+        text = preprocessString(row["text"])
+        title = preprocessString(row["title"])
 
-        pairs.append([text,title])
+        if(len(text.split(' ')) < MAX_LENGTH and len(title.split(' ')) < MAX_LENGTH):
+            data.append([text,title])
 
+            vocab.add_to_vocab(text)
+            vocab.add_to_vocab(title)
 
-    return pairs
-
-
-MAX_LENGTH = 600
-
-eng_prefixes = (
-    "i am ", "i m ",
-    "he is", "he s ",
-    "she is", "she s ",
-    "you are", "you re ",
-    "we are", "we re ",
-    "they are", "they re "
-)
-
-
-def filterPair(p):
-    return len(p[0].split(' ')) < MAX_LENGTH and \
-        len(p[1].split(' ')) < MAX_LENGTH
-
-
-def filterPairs(pairs):
-    return [pair for pair in pairs if filterPair(pair)]
-
-
-import random
-
-def prepareData():
-    pairs = read_news('./testing_data.json')
-    print("Read %s sentence pairs" % len(pairs))
-    pairs = filterPairs(pairs)
-    print("Trimmed to %s sentence pairs" % len(pairs))
-    print("Counting words...")
-    lang_model = Lang("english")
-    for pair in pairs:
-        lang_model.addSentence(pair[0])
-        lang_model.addSentence(pair[1])
-    print("Counted words:")
-    print(lang_model.name, lang_model.n_words)
-    return lang_model, pairs
-
-def prepareLangModel():
-    pairs = read_news('./training_data.json')
-    print("Read %s sentence pairs" % len(pairs))
-    pairs = filterPairs(pairs)
-    print("Trimmed to %s sentence pairs" % len(pairs))
-    print("Counting words...")
-    lang_model = Lang("english")
-    for pair in pairs:
-        lang_model.addSentence(pair[0])
-        lang_model.addSentence(pair[1])
-    print("Counted words:")
-    print(lang_model.name, lang_model.n_words)
-    return lang_model, pairs
+    return data , vocab
 
 
 
+_ , VOCAB_MODEL = read_data_and_vocab('./training_data.json')
+data , _ = read_data_and_vocab('./testing_data.json')
 
-lang_model, _ = prepareLangModel()
-_, pairs = prepareData()
-
-# random.shuffle(pairs)
-
-# cut = int(len(pairs)*0.8)
-# pairs_train = pairs[:cut]
-# pairs_test = pairs[cut:]
-
-# print(random.choice(pairs))
 
 
 
@@ -225,67 +171,11 @@ class AttnDecoderRNN(nn.Module):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
 
-
-def indexesFromSentence(lang, sentence):
-    return [lang.word2index[word] if word in lang.word2index else lang.word2index["and"] for word in sentence.split(' ')]
-
-
-def tensorFromSentence(lang, sentence):
-    indexes = indexesFromSentence(lang, sentence)
-    indexes.append(EOS_token)
-    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
-
-
-def tensorsFromPair(pair):
-    input_tensor = tensorFromSentence(lang_model, pair[0])
-#     print(input_tensor)
-#     print(input_tensor.shape)
-    target_tensor = tensorFromSentence(lang_model, pair[1])
-    return (input_tensor, target_tensor)
-
-
 teacher_forcing_ratio = 0.5
-
-def evaluate_no_attn(encoder, decoder, sentence, max_length=MAX_LENGTH):
-    with torch.no_grad():
-        input_tensor = tensorFromSentence(lang_model, sentence)
-        input_length = input_tensor.size()[0]
-        encoder_hidden = encoder.initHidden()
-
-        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
-
-        for ei in range(input_length):
-            encoder_output, encoder_hidden = encoder(input_tensor[ei],
-                                                     encoder_hidden)
-            encoder_outputs[ei] += encoder_output[0, 0]
-
-        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
-
-        decoder_hidden = encoder_hidden
-
-        decoded_words = []
-        decoder_attentions = torch.zeros(max_length, max_length)
-
-        for di in range(max_length):
-            decoder_output, decoder_hidden = decoder(
-                decoder_input, decoder_hidden)
-            #decoder_attentions[di] = decoder_attention.data
-            topv, topi = decoder_output.data.topk(1)
-            if topi.item() == EOS_token:
-                decoded_words.append('<EOS>')
-                break
-            else:
-                decoded_words.append(lang_model.index2word[topi.item()])
-
-            decoder_input = topi.squeeze().detach()
-
-        return decoded_words
-
-
 
 def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
     with torch.no_grad():
-        input_tensor = tensorFromSentence(lang_model, sentence)
+        input_tensor = VOCAB_MODEL.sentence_to_indexes(sentence)
         input_length = input_tensor.size()[0]
         encoder_hidden = encoder.initHidden()
 
@@ -312,7 +202,7 @@ def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
                 decoded_words.append('<EOS>')
                 break
             else:
-                decoded_words.append(lang_model.index2word[topi.item()])
+                decoded_words.append(VOCAB_MODEL.index2word[topi.item()])
 
             decoder_input = topi.squeeze().detach()
 
@@ -321,7 +211,7 @@ def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
 
 def evaluateRandomly(encoder, decoder, n=10):
     for i in range(n):
-        pair = random.choice(pairs)
+        pair = random.choice(data)
         print('>', pair[0])
         print('=', pair[1])
         output_words, attentions = evaluate(encoder, decoder, pair[0])
@@ -332,10 +222,9 @@ def evaluateRandomly(encoder, decoder, n=10):
 
 
 hidden_size = 256
-encoder2 = EncoderRNN(lang_model.n_words, hidden_size).to(device)
-attn_decoder2 = AttnDecoderRNN(hidden_size, lang_model.n_words, dropout_p=0.1).to(device)
-#attn_decoder2 = DecoderRNN(hidden_size, lang_model.n_words).to(device)
-encoder2.load_state_dict(torch.load('encoder_attn_collated.pth'))
-attn_decoder2.load_state_dict(torch.load('decoder_attn_collated.pth'))
+encoder1 = EncoderRNN(VOCAB_MODEL.n_words, hidden_size).to(device)
+attn_decoder1 = AttnDecoderRNN(hidden_size, VOCAB_MODEL.n_words, dropout_p=0.1).to(device)
+encoder1.load_state_dict(torch.load('encoder_attn_collated.pth'))
+attn_decoder1.load_state_dict(torch.load('decoder_attn_collated.pth'))
 
-evaluateRandomly(encoder2, attn_decoder2)
+evaluateRandomly(encoder1, attn_decoder1)
